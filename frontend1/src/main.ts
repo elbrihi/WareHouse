6374281bd05213b7e2e0994f6cc53698ce195c67
elbrihi <elbrihiyasine3@gmail.com>
2024-10-17 01:04:44 +0100

    ending securty frontend part

--- frontend1/src/main.ts
+++
@@ -1,6 +0,0 @@
-import 'zone.js';
-import { platformBrowser } from '@angular/platform-browser';
-
-import { AppModule } from './app/app.module';
-
-platformBrowser().bootstrapModule(AppModule).catch(console.error);
